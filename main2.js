697d320d724cd26d8b4404f7e9a21a235e44705d
Lucas Pereira <lucas_pereiracp@outlook.com.br>
2020-08-25 13:51:41 -0300

    Atividade 14, comentarios e main2.js

---
+++ main2.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const { DOMParser } = require('xmldom');
+const R = require('ramda');
+
+const { isValid, elementsToArray, getGitHubProject, contentOfAdded, contentOfUpdated} = require('./xmlfilter');
+
+// efetua o processamento do xml e armazena a estrutura no objeto 'document'
+const document = new DOMParser().parseFromString(fs.readFileSync('res/f-droid.xml', 'utf-8'));
+
+
+//RESULUÇÃO
+const isAddedAfter2000AndUpdatedAfter2020 = isValid(R.__, 2000, 2020);      
+
+const addedApps = elementsToArray(document.getElementsByTagName('application'))
+    .filter(isAddedAfter2000AndUpdatedAfter2020)
+    .map(getGitHubProject);
+
+const datas = elementsToArray(document.getElementsByTagName('application'))
+    .filter(isAddedAfter2000AndUpdatedAfter2020)
+    .map(contentOfAdded);
+
+const att = elementsToArray(document.getElementsByTagName('application'))
+    .filter(isAddedAfter2000AndUpdatedAfter2020)
+    .map(contentOfUpdated);
+
+const result = [];
+
+function results(array0, array1, array2, array3){
+
+    for(var i = 0; i < array3.length; i++ ){
+
+        let data = {
+            nome: array1[i],
+            Inclusao: array2[i],
+            Ultima_Atualizacao: array3[i]
+        }
+        array0[i] = data;                    
+    }
+}
+results(result, addedApps, datas, att);
+console.log(result);
+
+
+    
+ 
